45ec241797d9444e6bfd8dd345472cf4c5337a37
Andrey Varlamov <varlamov.and@gmail.com>
2023-03-06 00:43:32 -0800

    Map > Gen Rect room v2

--- src/map.c
+++ src/map.c
@@ -8,6 +8,34 @@ typedef struct Map
     bool blocked[SCREEN_COLS * SCREEN_ROWS];
 } Map;
 
+// to_fill should come preallocated and initialized
+void _map_gen_rooms(
+    unsigned char *to_fill, int map_width, int map_height,
+    int x, int y, int width, int height)
+{
+    if (x < 0 || y < 0 || x >= width || y >= height
+        || x + width > map_width || y + height > map_height
+    )
+    {
+        printf("_map_gen_rooms: WARNING: Room out of bounds. Skipping...");
+        return;
+    }
+
+    // Draw horizontal boundaries
+    for (int i = x; i < x + width; i++)
+    {
+        to_fill[util_xy_to_i(i, y, map_width)] = 1;
+        to_fill[util_xy_to_i(i, y + height - 1, map_width)] = 1;
+    }
+
+    // Draw vertical boundaries
+    for (int i = y + 1; i < y + height - 1; i++)
+    {
+        to_fill[util_xy_to_i(x, i, map_width)] = 1;
+        to_fill[util_xy_to_i(x + width - 1, i, map_width)] = 1;
+    }
+}
+
 void map_init(Map *map, int width, int height)
 {
     for (int i = 0; i <  width * height; i++)
@@ -17,36 +45,19 @@ void map_init(Map *map, int width, int height)
         glm_vec3_copy(GLM_VEC3_ZERO, map->bg_col[i]);
     }
 
-    int wall_tiles_count = width * 2 + height * 2 - 4;
-    int *wall_indeces = (int *) calloc(1, wall_tiles_count * sizeof(int));
+    unsigned char *walls = calloc(1, width * height * sizeof(unsigned char));
+    _map_gen_rooms(walls, width, height, 0, 0, width, height);
 
-    int wall_indeces_iter = 0;
-
-    // Draw horizontal boundaries
-    for (int i = 0; i < width; i++)
+    for (int i  = 0; i < width * height; i++)
     {
-        wall_indeces[wall_indeces_iter] = i;
-        wall_indeces_iter++;
-        wall_indeces[wall_indeces_iter] = util_xy_to_i(i, height - 1, width);
-        wall_indeces_iter++;
+        if (walls[i] == 1)
+        {
+            map->glyphs[i] = '#';
+            glm_vec3_copy((vec3) { 0.6f, 0.6f, 0.6f }, map->fg_col[i]);
+            glm_vec3_copy((vec3) { 0.7f, 0.7f, 0.7f }, map->bg_col[i]);
+            map->blocked[i] = true;
+        }
     }
 
-    // Draw vertical boundaries
-    for (int i = 1; i < height - 1; i++)
-    {
-        wall_indeces[wall_indeces_iter] = util_xy_to_i(0, i, width);
-        wall_indeces_iter++;
-        wall_indeces[wall_indeces_iter] = util_xy_to_i(width - 1, i, width);
-        wall_indeces_iter++;
-    }
-    
-    for (int i  = 0; i < wall_tiles_count; i++)
-    {
-        map->glyphs[wall_indeces[i]] = '#';
-        glm_vec3_copy((vec3) { 0.6f, 0.6f, 0.6f }, map->fg_col[wall_indeces[i]]);
-        glm_vec3_copy((vec3) { 0.7f, 0.7f, 0.7f }, map->bg_col[wall_indeces[i]]);
-        map->blocked[wall_indeces[i]] = true;
-    }
-
-    free(wall_indeces);
+    free(walls);
 }
